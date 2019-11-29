ebe1f2b8a1a0b40a424255a253609ccdcee6f029
Bartholomew Joyce <bartholomew.michael.joyce@gmail.com>
2019-11-29 16:34:30 +0100

    Bug fix: improper string truncation when text area smaller than ellipses

--- src/util/draw_text_in_box.cpp
+++ src/util/draw_text_in_box.cpp
@@ -44,29 +44,30 @@ int truncate_text(float width, int strlen, char* dst, const char* src) {
         return 0;
     }
 
+    // Get glyph positions
+    ddui::GlyphPosition glyph_positions[strlen];
+    auto num_glyph_positions = ddui::text_glyph_positions(0, 0, src, NULL, glyph_positions, strlen);
+
+    // Does the entire line fit?
+    auto text_width = glyph_positions[num_glyph_positions - 1].maxx - glyph_positions[0].minx;
+    if (text_width <= width) {
+        strcpy(dst, src);
+        return text_width + 2;
+    }
+
     // Measure width of ...
     float bounds[4];
     ddui::text_bounds(0, 0, "...", NULL, bounds);
     auto ellipses_width = bounds[2] - bounds[0];
-    
+
     // Handle less than ... width case
     if (width <= ellipses_width) {
         dst[0] = '\0';
         return 0;
     }
-    
-    // Get glyph positions
-    ddui::GlyphPosition glyph_positions[strlen];
-    auto num_glyph_positions = ddui::text_glyph_positions(0, 0, src, NULL, glyph_positions, strlen);
-    
+
     strcpy(dst, src);
-    
-    // Does the entire line fit?
-    auto text_width = glyph_positions[num_glyph_positions - 1].maxx - glyph_positions[0].minx;
-    if (text_width <= width) {
-        return text_width + 2;
-    }
-    
+
     // How much can we fit?
     auto num_chars = num_glyph_positions;
     for (; num_chars > 0; --num_chars) {
@@ -75,7 +76,7 @@ int truncate_text(float width, int strlen, char* dst, const char* src) {
             break;
         }
     }
-    
+
     // Append the dots
     dst[num_chars] = '.';
     dst[num_chars+1] = '.';
